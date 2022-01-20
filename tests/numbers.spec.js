0d7a7d4737726ad59c62f462252ccc060ef34323
Pérsio Pereira <persiopeo@gmail.com>
2022-01-20 16:11:31 -0300

    Adicionando o item-2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -15,11 +15,24 @@ const numbers = require('../src/numbers');
 */
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    fail('Teste vazio!');
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
+      it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
+        //fail('Teste vazio!');
+        // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+        // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+        // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+        // Escreva um teste em que a função recebe [' '] e retorna false
+      });
+      it('Testar se ao receber uma array de numeros retorna true', () => {
+          expect(numbers([1, 2, 3, 4, 5])).toBe(true);
+      })
+      it('Testar se ao receber uma array é um numero se não for retorna false', () => {
+          expect(numbers([4, "5"])).toBe(false);
+      })
+      it('Testar se ao receber uma array vazia retorna true', () => {
+          expect(numbers([])).toBe(true);
+      })
+      it('Testar se ao receber uma array e se tiver uma string retorna false', () => {
+          expect(numbers([2, 'errado', 5])).toBe(false);
+      })
   });
-});
+
